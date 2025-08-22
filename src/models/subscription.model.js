f71f7afb5b4f0464f4d808c3e87b1832caa3c762
Sarowar-h <sarw838dev@gmail.com>
2025-08-22 11:09:00 +0600

    feat: redefine subscription model with enhanced fields for plan management and client tracking

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,24 +1,46 @@
-const subscriptionPlanSchema = new Schema({
-  packageName: {
-    type: String,
-    required: true,
-    trim: true,
-  }, // e.g., "Plan 300"
-  maxClients: {
-    type: Number,
-    required: true,
-  }, // PPP user limit
-  price: {
-    type: Number,
-    required: true,
-  }, // Price in BDT
-  billingCycle: {
-    type: String,
-    default: "Monthly",
-  },
-});
+import { Schema, model } from "mongoose";
 
-export const SubscriptionPlan = model(
-  "SubscriptionPlan",
-  subscriptionPlanSchema
+const subscriptionSchema = new Schema(
+  {
+    planName: {
+      // brounch, silver, gold, dimond
+      type: String,
+      required: true,
+      trim: true,
+    },
+    maxClients: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    billingCycle: {
+      type: String,
+      default: "Monthly",
+    },
+    // belongs only to Admin
+    adminId: {
+      type: Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    usedClients: {
+      type: Number,
+      default: 0, // total active clients under all resellers of this admin
+    },
+    expiryDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "expired", "suspended"],
+      default: "active",
+    },
+  },
+  { timestamps: true } // starting date is in it.
 );
+
+export const Subscription = model("Subscription", subscriptionSchema);
